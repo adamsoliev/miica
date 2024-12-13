a9f06793238c65d4ce5d6cdc362b7460651848e8
Adam Soliev <asoliyev13@gmail.com>
2024-12-13 01:05:31 -0600

    add skeleton and parts

--- src/4bit/main.py
+++ src/4bit/main.py
@@ -1,8 +1,32 @@
 #! /usr/bin/python3
 
+RAM = bytearray(640)  # data memory | each 4002 RAM chip is 40-byte
+
+ROM = bytearray(4096) # program memory | each 4001 ROM chip is 256-byte
+
+PROGRAM_COUNTER = bytearray(12)         # pc
+STACK_REGISTERS = [bytearray(12)] * 3   # stack
+INDEX_REGISTERS = [bytearray(4)] * 16   # registers
+ACCUMULATOR = bytearray(4)              
+TEMPORARY_REGISTER = bytearray(4)       # carry?
+INSTRUCTION_REGISTER = bytearray(8)     
+
+def fetch():
+    return 
+
+def decode(string):
+    return
+
+def execute(opcode, operands):
+    return
+
+def loop():
+    for i in range(100):
+        print("4004 is running")
+    return
 
 def main():
-    print("Hello, World!")
+    loop()
 
 if __name__ == "__main__":
     main()
